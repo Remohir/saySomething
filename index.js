73fc2749f3478f1621b63dd482cc754827c06ee5
Ezequiel Pequeño Calvar <contact@ezedeveloper.com>
2017-09-11 01:29:02 +0100

    Create arrays to store input values
    In order to use them in the future

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var value = document.getElementById("say");
 var show = document.getElementById("show");
+var arr1 = [];
+var arr2 = [];
 
 function play() {
 	value.addEventListener("keydown", function(e) {	
 		if(e.which == 13 || e.keyCode == 13) {
 			e.preventDefault();
+			arr1.push(value.value);
+			console.log(arr1);
 			show.innerHTML = value.value;
 			value.value = "";
 		}
