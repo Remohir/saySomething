8e4bfd5fff6631f48e93e0e135497a199d812471
Ezequiel Pequeño Calvar <contact@ezedeveloper.com>
2017-09-14 23:54:12 +0100

    Add counter js, update html and css
    HTML: p tag for the counter added
    CSS: p tage #counter styled and display { visibility: hidden } deleted
    JS: Counter functionality added (minor bug needs to be fixed on firs enter press)

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var value = document.getElementById("say");
 var show = document.getElementById("show");
 var main = document.getElementById("main");
+var counter = document.getElementById("counter");
+var count = 500;
 var arr1 = ["Hello", "I", "am", "a", "test"];
 var arr2 = [];
 var number = arr1.length;
 var newNumber;
+counter.innerHTML = count;
 
 
 play();
+remaining();
 
 
 function play() {
 	value.addEventListener("keydown", function(e) {
 		if(main.id == "main") {	
 			if((e.which == 13 || e.keyCode == 13) && (value.value != "")) {
+				counter.innerHTML = count;
 				e.preventDefault();
 				arr1.push(value.value);
 				console.log(arr1);
@@ -22,13 +27,12 @@ function play() {
 				console.log(main);
 				main.removeAttribute("id");
 				main.classList.add("head");
-				show.classList.remove("hide");
+				setInterval(display, 1500);
 			}
 		} else {
 			keepPushing();
 		}
 	});
-	setInterval(display, 1500);
 }
 
 function display() {
@@ -46,11 +50,24 @@ function display() {
 }
 
 function keepPushing(e) {
-	value.addEventListener("keydown", function(e) {
+	value.addEventListener("keyup", function(e) {
 		if((e.which == 13 || e.keyCode == 13) && (value.value != "")) {
 			arr1.push(value.value);
 			value.value = "";
 			console.log(arr1);
 		}
+	});
+}
+
+function remaining() {
+	value.addEventListener("keyup", function(e) {
+		if((e.which != 13 || e.keyCode != 13) && (value.value.length != 0) && (e.which !=  8 ||  e.keyCode != 8 ) && (e.which !=  46 ||  e.keyCode != 46 )) {
+			count = 500 - value.value.length;
+		} else if ((e.which ==  8 ||  e.keyCode == 8 ) || (e.which ==  46 ||  e.keyCode == 46 ) && (value.value != "")) {
+			count = 500 - value.value.length;
+		} else if((e.which == 13 || e.keyCode == 13) && (value.value != "")) {
+			count = 500;
+		}
+		counter.innerHTML = count;
 	});
 }
